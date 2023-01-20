544d26c3f3ee43331dcb4cb5f6304d8d6e555d75
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-01-20 01:24:33 -0500

    trying something for path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
 module.exports = {
   assetPrefix: "",
   basePath: "/",
   images: {
     loader: 'akamai',
     path: '',
-  },
+  }
 }
 
 export default nextConfig;
