ecccfa11a64728f6126fc7c3e18e6902a68a4cd0
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-01-20 01:18:34 -0500

    gh-pages set-up

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
-/**
- * @type {import('next').NextConfig}
- */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-const nextConfig = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
-};
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
 
 export default nextConfig;
